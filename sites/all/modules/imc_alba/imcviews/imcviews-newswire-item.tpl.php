9d3b377666097388bb2b1ec3c53b2a63fca6b5fd
tom <tomm@riseup.net>
2009-04-20 21:53:36 +0100

    fix broken newswire crappy meaningless media type icons

--- sites/all/modules/imc_alba/imcviews/imcviews-newswire-item.tpl.php
+++ sites/all/modules/imc_alba/imcviews/imcviews-newswire-item.tpl.php
@@ -1,3 +1,3 @@
 <div class="newswire-link">
-<?php print '<img src="'.$imgpath.'/text_small.gif'.'" /> '.l($title, 'node/' . $node->nid); ?>
+<?php print '<img src="'.$imgpath.'" /> '.l($title, 'node/' . $node->nid); ?>
 </div>
